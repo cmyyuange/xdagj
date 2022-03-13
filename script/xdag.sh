374dd8f265ec46b4065e1c84d45f05b11a94eadd
MyronX <513482380@qq.com>
2022-03-13 15:26:31 +0800

    [mod] fixbug & support bash

--- script/xdag.sh
+++ script/xdag.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 # =========================  constant start  =========================
 # hint
-XDAG_INFO="\033[34m [INFO] \033[0m"
-XDAG_WARNING="\033[33m [WARNING] \033[0m"
-XDAG_ERROR="\033[31m [ERROR] \033[0m"
+XDAG_INFO="\033[34m[XDAG_INFO] \033[0m"
+XDAG_WARNING="\033[33m[XDAG_WARNING] \033[0m"
+XDAG_ERROR="\033[31m[XDAG_ERROR] \033[0m"
 
 # default JVM options
 JAVA_OPTS="--add-opens java.base/java.nio=ALL-UNNAMED --add-opens java.base/sun.nio.ch=ALL-UNNAMED -server -Xms1g -Xmx1g"
@@ -20,10 +20,14 @@ XDAG_TESTNET_CONFIG_NAME="xdag-testnet.config"
 XDAG_MAINNET_CONFIG_NAME="xdag-mainnet.config"
 XDAG_CONFIG_NAME=$XDAG_MAINNET_CONFIG_NAME
 
+# for maven
+XDAG_MAVEN_OPTS=""
+
 # optional startup parameters for getopts
 USAGE="m"  # use mainnet
 USAGE+="t" # use testnet
 USAGE+="h" # help Print usage
+USAGE+="D" # jump test when build maven
 # =========================  constant end  =========================
 
 # =========================  function start  =========================
@@ -36,18 +40,26 @@ function getXdagJarName() {
 
     # Get the name of the jar package available for execution in the target directory
     targetPath="$rootPath/target/"
-    if [ ! -d "$targetPath" ]; then
-        # If the target directory is missing, do package
-        echo "\033[47;30m============  Build XDAGJ Project Start============\033[0m"
-        buildXdagProject ${rootPath}
-        echo "\033[47;30m============  Build XDAGJ Project Finish ============\033[0m"
+
+    # 如果文件存在，并且可以找到 jar 包
+    if [ -d "${targetPath}" ]; then
+        cd $targetPath
+        if [ ! -z "$(ls ${XDAG_JAR_REGEX})" ]; then
+            XDAG_JAR_NAME=$(ls ${XDAG_JAR_REGEX})
+            echo -e "${XDAG_INFO}Find jar [${XDAG_JAR_NAME}]"
+            return
+        fi
     fi
+
+    echo -e "${XDAG_INFO}Project not building"
+    cd $rootPath
+    # build project
+    echo -e "\033[47;30m============  Build XDAGJ Project Start============\033[0m"
+    buildXdagProject ${rootPath}
+    echo -e "\033[47;30m============  Build XDAGJ Project Finish ============\033[0m"
+
     cd $targetPath
     XDAG_JAR_NAME=$(ls ${XDAG_JAR_REGEX})
-    if [ -z XDAG_JAR_NAME ]; then
-        echo "${XDAG_ERROR}The jar is missing in the target directory and cannot be run, please check"
-        exit 1
-    fi
 }
 
 function buildXdagProject() {
@@ -56,7 +68,7 @@ function buildXdagProject() {
     randomXBuildFloderName="build"
 
     cd "${randomXBuildPath}"
-    echo "\033[47;30m============  Build RandomX Lib Start  ============\033[0m"
+    echo -e "\033[47;30m============  Build RandomX Lib Start  ============\033[0m"
 
     # does /src/c/build exist
     if [ ! -d "${randomXBuildFloderName}" ]; then
@@ -65,48 +77,49 @@ function buildXdagProject() {
     cd ${randomXBuildFloderName}
     if [ $? -ne 0 ]; then
         # Failed to create build file
-        echo "${XDAG_ERROR}Unable to create folder for RandomX script,The wrong path [${randomXBuildPath} ] please check"
+        echo -e "${XDAG_ERROR}Unable to create folder for RandomX script,The wrong path [${randomXBuildPath} ] please check"
         exit 1
     fi
     cmake ..
     make
     if [ $? -ne 0 ]; then
-        echo "${XDAG_ERROR}Build RandomX Lib failed"
+        echo -e "${XDAG_ERROR}Build RandomX Lib failed"
         exit 1
     fi
-    echo "\033[47;30m============  Build RandomX Lib Finish & Success ============\033[0m"
+    echo -e "\033[47;30m============  Build RandomX Lib Finish & Success ============\033[0m"
     echo ""
-    echo "\033[47;30m============  Build JAR Start  ============\033[0m"
+    echo -e "\033[47;30m============  Build JAR Start  ============\033[0m"
 
     cd ${rootPath}
-    mvn clean package
+    mvn clean package ${XDAG_MAVEN_OPTS}
     if [ $? -ne 0 ]; then
-        echo "${XDAG_ERROR}Build JAR failed"
+        echo -e "${XDAG_ERROR}Build JAR failed"
         exit 1
     fi
-    echo "\033[47;30m ============  Build JAR Finish & Success  ============ \033[0m"
+    echo -e "\033[47;30m ============  Build JAR Finish & Success  ============ \033[0m"
 }
 
 # $1 filename:  the file need to be copy
 # $2 originalFilePath: the original path of file
 # describe:  Determine whether there is file in the current directory, if it does not exist, copy it to the original path
 function copyFile() {
+
     fileName=$1
     OriginalFilePath=$2
 
     # Check if the file exists in the current directory
     if [ ! -f "$fileName" ]; then
-        echo "${XDAG_INFO}lack of [ $fileName ],  waiting for the copy"
+        echo -e "${XDAG_INFO}lack of [ $fileName ],  waiting for the copy"
         temp="${OriginalFilePath}/${fileName}"
         cp ${temp} ./
         # Whether the copy was successful
         if [ $? -ne 0 ]; then
-            echo "${XDAG_ERROR}copy ${fileName} failed"
+            echo -e "${XDAG_ERROR}copy ${fileName} failed"
         else
-            echo "${XDAG_INFO}copy ${fileName} success"
+            echo -e "${XDAG_INFO}copy ${fileName} success"
         fi
     else
-        echo "${XDAG_INFO}The file [$fileName] are ready"
+        echo -e "${XDAG_INFO}The file [$fileName] are ready"
     fi
 
 }
@@ -117,13 +130,13 @@ function copyFile() {
 while getopts "$USAGE" opt; do
     case $opt in
     m)
-        echo "${XDAG_INFO}Prepare to start the main network pool"
+        echo -e "${XDAG_INFO}Prepare to start the main network pool"
         # add startup parameters
         XDAG_OPTS+="-m"
         XDAG_CONFIG_NAME=$XDAG_MAINNET_CONFIG_NAME
         ;;
     t)
-        echo "${XDAG_INFO}Prepare to start the test network pool"
+        echo -e "${XDAG_INFO}Prepare to start the test network pool"
         XDAG_OPTS+="-t"
         XDAG_CONFIG_NAME=$XDAG_TESTNET_CONFIG_NAME
         ;;
@@ -132,11 +145,15 @@ while getopts "$USAGE" opt; do
         echo "options:"
         echo "\\t-t: use testnet"
         echo "\\t-m: use maintest"
+        echo "\\t-D: skip tests during maven build"
         echo "\\t-h: print help"
         exit 0
         ;;
+    D)
+        XDAG_MAVEN_OPTS+="-Dmaven.test.skip=true"
+        ;;
     ?)
-        echo "${XDAG_ERROR}unknown parameters $opt"
+        echo -e "${XDAG_ERROR}unknown parameters $opt"
         exit 1
         ;;
     esac
@@ -145,9 +162,9 @@ done
 # If you are using the release folder, there should already be a corresponding jar package and configuration file in the current directory
 if (test ! -z "$(ls ${XDAG_JAR_REGEX} >/dev/null 2>&1)") && (test ! -z "$(ls ${XDAG_CONFIG_NAME} >/dev/null 2>&1)"); then
     XDAG_JAR_NAME="$(ls ${XDAG_JAR_REGEX})"
-    echo "${XDAG_INFO}The file [${XDAG_JAR_NAME}] are ready"
-    echo "${XDAG_INFO}The file [${XDAG_CONFIG_NAME}] are ready"
-    echo "\033[42;37m============  Mining Pool Is Starting......  ============\033[0m"
+    echo -e "${XDAG_INFO}The file [${XDAG_JAR_NAME}] are ready"
+    echo -e "${XDAG_INFO}The file [${XDAG_CONFIG_NAME}] are ready"
+    echo -e "\033[42;37m============  Mining Pool Is Starting......  ============\033[0m"
     java ${JAVA_OPTS} -cp .:$(ls ${XDAG_JAR_NAME}) io.xdag.Bootstrap "${XDAG_OPTS}"
     exit 0
 fi
@@ -159,16 +176,16 @@ getXdagJarName
 XDAG_POOL_PATH="${rootPath}/${XDAG_POOL_FOLDER_NAME}"
 # Check if the file exists, and create it if it doesn't
 if [ ! -d "$XDAG_POOL_PATH" ]; then
-    echo "${XDAG_INFO}create folder [ $XDAG_POOL_PATH ]"
+    echo -e "${XDAG_INFO}create folder [ $XDAG_POOL_PATH ]"
     mkdir "$XDAG_POOL_PATH"
     if [ $? -ne 0 ]; then
-        echo "${XDAG_ERROR}create pool folder failed"
+        echo -e "${XDAG_ERROR}create pool folder failed"
         exit 1
     else
-        echo "${XDAG_INFO}create pool folder success"
+        echo -e "${XDAG_INFO}create pool folder success"
     fi
 else
-    echo "${XDAG_INFO}folder [ $XDAG_POOL_PATH ] are ready"
+    echo -e "${XDAG_INFO}folder [ $XDAG_POOL_PATH ] are ready"
 fi
 
 cd $XDAG_POOL_PATH
@@ -178,5 +195,5 @@ copyFile $XDAG_CONFIG_NAME "${rootPath}/src/main/resources"
 copyFile "netdb-*.txt" "${rootPath}/src/main/resources"
 
 # launch
-echo "\033[42;37m============  Mining Pool Is Starting......  ============\033[0m"
+echo -e "\033[42;37m============  Mining Pool Is Starting......  ============\033[0m"
 java ${JAVA_OPTS} -cp .:${XDAG_JAR_NAME} io.xdag.Bootstrap "${XDAG_OPTS}"
